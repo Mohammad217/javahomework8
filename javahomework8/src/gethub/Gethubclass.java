ea2641cccdc334c8a6511da94db83fb2fabcbf0e
alaud <alaud@DESKTOP-39OEN0A>
2020-06-09 13:41:41 -0400

    link google page

--- javahomework8/src/gethub/Gethubclass.java
+++ javahomework8/src/gethub/Gethubclass.java
@@ -1,9 +1,26 @@
 package gethub;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Gethubclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\alaud\\eclipse-workspace\\Jars\\chromedriver_win32\\geckodriver.exe"); 
+		WebDriver driver = new FirefoxDriver(); //Creating an object of FirefoxDriver
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("Gethub"); //name locator for text box
+		WebElement searchbutton = driver.findElement(By.name("btnK"));//name locator for google search
+		searchbutton.click();
+		driver.quit();
 
 	}
 
