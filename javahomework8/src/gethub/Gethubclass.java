34fece218f3dab5efac88eee3ab43ad7b22075c6
alaud <alaud@Mohammad-uddin>
2020-06-15 15:54:37 -0400

    add google

--- javahomework8/src/gethub/Gethubclass.java
+++ javahomework8/src/gethub/Gethubclass.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -13,15 +14,15 @@ public class Gethubclass {
 	@Test
 
 	public void openBrowers() {
-		System.setProperty("webdriver.gecko.driver","C:\\Users\\alaud\\eclipse-workspace\\Jars\\chromedriver_win32\\geckodriver.exe"); 
-		WebDriver driver = new FirefoxDriver(); //Creating an object of FirefoxDriver
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\alaud\\eclipse-workspace\\Jars\\chromedriver_win32\\chromedriver.exe"); 
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://www.google.com/");
-		driver.findElement(By.name("q")).sendKeys("Gethub"); //name locator for text box
-		WebElement searchbutton = driver.findElement(By.name("btnK"));//name locator for google search
+		driver.findElement(By.name("q")).sendKeys("facebook"); 
+		WebElement searchbutton = driver.findElement(By.name("btnK"));
 		searchbutton.click();
 		driver.quit();
 
