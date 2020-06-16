be1d37d7ef4c2a7a3f7aae81afe11d86ac4b677d
alaud <alaud@Mohammad-uddin>
2020-06-16 17:04:43 -0400

    added getCurrentUrl/get Title

--- javahomework8/src/gethub/Gethubclass.java
+++ javahomework8/src/gethub/Gethubclass.java
@@ -17,6 +17,8 @@ public class Gethubclass {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\alaud\\eclipse-workspace\\Jars\\chromedriver_win32\\chromedriver.exe"); 
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		 System.out.println(driver.getCurrentUrl());
+	     System.out.println(driver.getTitle());
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
